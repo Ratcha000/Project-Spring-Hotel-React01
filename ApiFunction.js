c0f822e12af5e5876fc876bb4d5bae5db8e1df05
thanawin597-2 <thanawin.p@kkumail.com>
2025-09-21 17:30:24 +0700

    Update ApiFunction.js
    export function getBookingByConfirmationCode

--- ApiFunction.js
+++ ApiFunction.js
@@ -19,3 +19,16 @@ export async function getAllBookings() {
         throw new Error('Error fetching bookings : ${error.message}')
     }
 }
+
+export async function getBookingByConfirmationCode(confirmationCode) {
+    try{
+        const result = await api.get('/bookings/confirmation/${confirmationCode}')
+        return result.data
+    }catch(error){
+        if(error.response && error.response.data){
+            throw new Error(error.response.data)
+        }else{
+            throw new Error('Error find booking : ${error.message}')
+        }
+    } 
+}
