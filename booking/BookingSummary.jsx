57dc61f190ccf8a39787f817356b4788b31be526
thanawin597-2 <thanawin.p@kkumail.com>
2025-09-21 19:19:11 +0700

    Update BookingSummary.jsx
    create  useEffect

--- booking/BookingSummary.jsx
+++ booking/BookingSummary.jsx
@@ -1,12 +1,15 @@
-import React from "react"
+import React, { useEffect } from "react"
 import React, {useState} from "react"
+import { useNavigate } from 'react-router-dom'
 
 const BookingSummary = ({booking, payment, isFormValid, onConfirm}) => {
     const checkInDate = moment(booking.checkInDate)
     const checkOutDate = moment(booking.checkInDate)
     const numOfDays = checkOutDate.diff(checkInDate, "days")
     const[isBookingConfirmed, setIsBookingConfirmed] = useState(false)
-    const[isProcessingPayment, setIsBookingPayment] = useState(false)
+    const[isProcessingPayment, setIsProcessingPayment] = useState(false)
+
+    const navigate = useNavigate()
 
     const handleConfirmBooking =() =>{
         setISProcessingPayment(true)
@@ -17,6 +20,13 @@ const BookingSummary = ({booking, payment, isFormValid, onConfirm}) => {
         }, 3000)
     }
 
+    useEffect(() => {
+        if(isBookingConfirmed){
+            navigate("/booking-success")
+        }
+    }, [isBookingConfirmed, navigate])
+
+    
     return (
         <div>
 
