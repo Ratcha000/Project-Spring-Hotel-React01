8eb0341e1d7e5d5bad36c3b0b28cf26cb3362d90
thanawin597-2 <thanawin.p@kkumail.com>
2025-09-21 19:08:23 +0700

    Update BookingSummary.jsx
    create const BookingSummary

--- booking/BookingSummary.jsx
+++ booking/BookingSummary.jsx
@@ -1,1 +1,20 @@
+import React from "react"
+import React, {useState} from "react"
 
+const BookingSummary = ({booking, payment, isFormValid, onConfirm}) => {
+    const checkInDate = moment(booking.checkInDate)
+    const checkOutDate = moment(booking.checkInDate)
+    const numOfDays = checkOutDate.diff(checkInDate, "days")
+    const[isBookingConfirmed, setIsBookingConfirmed] = useState(false)
+    const[isProcessingPayment, setIsBookingPayment] = useState(false)
+
+    
+
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default BookingSummary
